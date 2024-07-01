be44b1ff92f22171bef012fb6dcd74a54577e314
emerson452 <emerson@popoyoko.fr>
2024-07-01 13:25:33 +0200

    ✨ starting api and initial features

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Groupe-5-Web-3-2024/flash-mcqueen-server
 
 go 1.21.3
+
+require github.com/eclipse/paho.mqtt.golang v1.4.3 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,51 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+    "log"
+    "github.com/eclipse/paho.mqtt.golang"
+)
+
+var mqttClient mqtt.Client
 
 func main() {
-	fmt.Println("Ka Chow !")
+    opts := mqtt.NewClientOptions().AddBroker("tcp://broker.hivemq.com:1883")
+    mqttClient = mqtt.NewClient(opts)
+    if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
+        log.Fatal(token.Error())
+    }
+
+    http.HandleFunc("/forward", handleForward)
+    http.HandleFunc("/backward", handleBackward)
+    http.HandleFunc("/left", handleLeft)
+    http.HandleFunc("/right", handleRight)
+
+    fmt.Println("Serveur démarré sur le port 8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+func handleForward(w http.ResponseWriter, r *http.Request) {
+    sendCommand("F")
+    fmt.Fprintf(w, "moving forward")
+}
+
+func handleBackward(w http.ResponseWriter, r *http.Request) {
+    sendCommand("B")
+    fmt.Fprintf(w, "moving backward")
+}
+
+func handleLeft(w http.ResponseWriter, r *http.Request) {
+    sendCommand("L")
+    fmt.Fprintf(w, "turning left")
+}
+
+func handleRight(w http.ResponseWriter, r *http.Request) {
+    sendCommand("R")
+    fmt.Fprintf(w, "turning right")
+}
+
+func sendCommand(command string) {
+    token := mqttClient.Publish("rc_car/command", 0, false, command)
+    token.Wait()
+}
